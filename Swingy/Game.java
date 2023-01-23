ee4b6eea7d4c03f2bb224d61439cd751aa3a1263
S-i-d-V <utheveny42@gmail.com>
2023-01-23 23:57:01 +0100

    Maven init

--- Swingy/Game.java
+++
@@ -1,9 +0,0 @@
-package Swingy;
-
-public class Game {
-    
-    public static void main(String[] args) {
-        for (int i = 0; i < 5; i++)
-            System.out.println("Game loop");
-    }
-}
