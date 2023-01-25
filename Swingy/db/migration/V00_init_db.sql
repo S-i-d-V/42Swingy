74aa284788d7978fc5001900c30c90bd71a0bc07
S-i-d-V <utheveny42@gmail.com>
2023-01-25 15:30:23 +0100

    Struggling to connect to the db with flyway, trying with H2 database instead of mysql

--- Swingy/db/migration/V00_init_db.sql
+++
@@ -1,4 +0,0 @@
-CREATE DATABASE IF NOT EXISTS swingy_db;
-CREATE USER 'ugtheven'@'localhost:933' IDENTIFIED BY '123456';
-GRANT ALL PRIVILEGES ON swingy_db.* TO 'ugtheven'@'localhost:933';
-FLUSH PRIVILEGES;
